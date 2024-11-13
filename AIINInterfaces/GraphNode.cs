94e4b5dc4bdc9842a8ba69a6d21371e753b9a3f1
Jan Zakrzewski <janzak214@gmail.com>
2024-11-13 16:03:47 +0100

    Change GraphNode to include original ids

--- AIINInterfaces/GraphNode.cs
+++ AIINInterfaces/GraphNode.cs
@@ -1,9 +1,28 @@
-﻿namespace AIINInterfaces;
+﻿using Geo;
 
-public record GraphNode(List<(Node, float)> ConnectedNodes)
+namespace AIINInterfaces;
+
+public record GraphNode(long Id, List<(GraphNode node, double weight)> ConnectedNodes, Coordinate Position)
 {
+    public override string ToString()
+    {
+        var connections = string.Join(", ", ConnectedNodes.Select(x => $"({x.node.Id}, {x.weight}m)"));
+        return $"node({Id}, [{connections}])";
+    }
 }
 
-public record ParcelLockerGraphNode(List<(Node, float)> ConnectedNodes) : GraphNode(ConnectedNodes)
+public record ParcelLockerGraphNode(
+    long Id,
+    List<(GraphNode node, double weight)> ConnectedNodes,
+    Coordinate Position,
+    long ParcelLockerId,
+    Coordinate ParcelLockerPosition
+)
+    : GraphNode(Id, ConnectedNodes, Position)
 {
+    public override string ToString()
+    {
+        var connections = string.Join(", ", ConnectedNodes.Select(x => $"({x.node.Id}, {x.weight}m)"));
+        return $"node(pl:{ParcelLockerId}, {Id}, [{connections}])";
+    }
 }
