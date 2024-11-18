64a266dd630ffba09fca08018993c3bdbcd57526
Jakub Wadrzyk <92339252+Shikoqu@users.noreply.github.com>
2024-11-18 01:01:20 +0100

    [fix] extract IsExtensions

--- AIINLib.Tests/RoadGraphBuilderTest.cs
+++ AIINLib.Tests/RoadGraphBuilderTest.cs
@@ -1,53 +1,12 @@
 using AIINInterfaces;
 using Geo;
-using NUnit.Framework.Constraints;
-
-// ReSharper disable AccessToStaticMemberViaDerivedType
 
 namespace AIINLib.Test;
-
-class ConnectedConstraint(GraphNode expected, bool twoWay) : Constraint
-{
-    public override ConstraintResult ApplyTo<TActual>(TActual actual)
-    {
-        if (actual is not GraphNode node)
-        {
-            return new ConstraintResult(this, actual, ConstraintStatus.Error);
-        }
-
-        var connected = node.ConnectedNodes.Any(x => x.Item1.Id == expected.Id);
-        if (!twoWay)
-        {
-            return new ConstraintResult(this, actual, connected ? ConstraintStatus.Success : ConstraintStatus.Failure);
-        }
-
-        var connectedBack = expected.ConnectedNodes.Any(x => x.Item1.Id == node.Id);
-        return new ConstraintResult(this, actual,
-            connected && connectedBack ? ConstraintStatus.Success : ConstraintStatus.Failure);
-    }
-}
-
-class Is : NUnit.Framework.Is
-{
-    public static ConnectedConstraint ConnectedTo(GraphNode expected, bool twoWay = true)
-    {
-        return new ConnectedConstraint(expected, twoWay);
-    }
-}
-
-static class IsExtensions
-{
-    public static ConnectedConstraint ConnectedTo(this ConstraintExpression expression, GraphNode expected,
-        bool twoWay = true)
-    {
-        return new ConnectedConstraint(expected, twoWay);
-    }
-}
 
 [TestFixture]
 public class RoadGraphBuilderTest
 {
-    private void ConnectNodes(GraphNode node1, GraphNode node2, double distance)
+    private static void ConnectNodes(GraphNode node1, GraphNode node2, double distance)
     {
         node1.ConnectedNodes.Add((node2, distance));
         node2.ConnectedNodes.Add((node1, distance));
