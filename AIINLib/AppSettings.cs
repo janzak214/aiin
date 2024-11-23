dc6b43710576dfbfe6d82437bbbe211fe6958d29
Jakub Wadrzyk <92339252+Shikoqu@users.noreply.github.com>
2024-11-23 18:26:24 +0100

    add AppSettings

---
+++ AIINLib/AppSettings.cs
@@ -0,0 +1,12 @@
+namespace AIINLib;
+
+
+public static class AppSettings
+{
+    public static double MutationRate = 0.1;
+    public static double CrossoverRate = 0.7;
+    
+    public static int PopulationSize = 100;
+    public static int TournamentSize = 5;
+    public static int MaxGenerations = 1000;
+}
